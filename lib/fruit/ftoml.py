cce617ee9eb543f32de94d54d53cd5f9b7f15bd3
Tomas Ebenlendr <ebik@ucw.cz>
2019-05-12 22:52:55 +0200

    Allow TOML inline object decoding.

--- lib/fruit/ftoml.py
+++ lib/fruit/ftoml.py
@@ -57,6 +57,39 @@ else:
                 return self.FruTypes[sv[0]](retv), rett
             return super(FruTomlDecoder, self).load_value(v, strictly_valid)
 
+        def load_inline_object(self, line, currentlevel, multikey=False,
+                               multibackslash=False):
+            candidate_groups = line[1:-1].split(",")
+            groups = []
+            if len(candidate_groups) == 1 and not candidate_groups[0].strip():
+                candidate_groups.pop()
+            while len(candidate_groups) > 0:
+                candidate_group = candidate_groups.pop(0)
+                try:
+                    _, value = candidate_group.split('=', 1)
+                except ValueError:
+                    raise ValueError("Invalid inline table encountered")
+                value = value.strip()
+                if ((value[0] == value[-1] and value[0] in ('"', "'")) or
+                    value[0] in '-0123456789' or
+                    value in ('true', 'false') or
+                    (value[0] == "[" and value[-1] == "]") or
+                    (value[0] == '{' and value[-1] == '}') or
+                    (value[0] in self.FruTypes and value[1:2] == value[-1]
+                        and value[-1] in ('"', '"')) ):
+                    groups.append(candidate_group)
+                elif len(candidate_groups) > 0:
+                    candidate_groups[0] = (candidate_group + "," +
+                                           candidate_groups[0])
+                else:
+                    raise ValueError("Invalid inline table value encountered")
+            for group in groups:
+                status = self.load_line(group, currentlevel, multikey,
+                                        multibackslash)
+                if status is not None:
+                    break
+
+
     def dump(cfg):
         return toml.dumps(cfg, FruTomlEncoder()).encode('utf8')
 
