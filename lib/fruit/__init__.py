2e64409928023b81c7f22ad52af455b366a758b3
Tomas Ebenlendr <ebik@ucw.cz>
2019-05-12 21:10:38 +0200

    TOML is not needed for library.

--- lib/fruit/__init__.py
+++ lib/fruit/__init__.py
@@ -1,4 +1,8 @@
-from .ftoml   import dump, load
 from .decoder import decode
 from .encoder import encode
 from .logging import *
+# TOML is not required for library
+try:
+    from .ftoml   import dump, load
+except ImportError:
+    pass
